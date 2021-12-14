e50917f61b41931e496b7d190a1a23f1cb1454a3
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-14 16:40:55 +0200

    Update 11. Sum of Odd Numbers.js

--- 01. Basic-Syntax-Conditional-Statements-and-Loops/Lab/11. Sum of Odd Numbers.js
+++ 01. Basic-Syntax-Conditional-Statements-and-Loops/Lab/11. Sum of Odd Numbers.js
@@ -1,5 +1,15 @@
-function sumOfOddNumbers() {
+function sumOfOddNumbers(n) {
 
+    let sum = 0;
+    let num = 1;
 
+    for (let i = 0; i < n; i++) {
+
+        console.log(num);
+
+        sum += num;
+        num += 2;
+    }
+    console.log(`Sum: ${sum}`);
 }
-sumOfOddNumbers();
+sumOfOddNumbers(5);
