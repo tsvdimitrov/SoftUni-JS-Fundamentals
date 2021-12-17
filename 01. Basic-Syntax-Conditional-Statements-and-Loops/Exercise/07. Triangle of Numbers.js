5b0b43288b8c0a40ace1d5b9a64c9e0b8e595414
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-17 17:25:08 +0200

    Create 08. Multiplication Table.js

--- 01. Basic-Syntax-Conditional-Statements-and-Loops/Exercise/07. Triangle of Numbers.js
+++ 01. Basic-Syntax-Conditional-Statements-and-Loops/Exercise/07. Triangle of Numbers.js
@@ -1,5 +1,11 @@
-function triangleOfNumbers() {
+function triangleOfNumbers(number) {
 
-
+    for (let i = 1; i <= number; i++) {
+        let result = '';
+        for (let j = 1; j <= i; j++) {
+            result += ` ${i}`;
+        }
+        console.log(result);
+    }
 }
 triangleOfNumbers(3);
