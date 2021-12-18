27476774fca9f6983f1e331e5dfc7eb22fdbe542
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-18 12:36:39 +0200

    Create 09. Login.js

---
+++ 01. Basic-Syntax-Conditional-Statements-and-Loops/Exercise/09. Login.js
@@ -0,0 +1,28 @@
+function login(list) {
+
+    let username = list.shift();
+    let passwordAsList = username.split('');
+    let passwordAsListReversed = passwordAsList.reverse();
+    let password = passwordAsListReversed.join('');
+
+    let counter = 1;
+
+    while (true) {
+        let enteredPassword = list.shift('');
+
+        if (counter == 4) {
+            console.log(`User ${username} blocked!`);
+            break;
+        }
+
+        if (enteredPassword == password) {
+            console.log(`User ${username} logged in.`);
+            break;
+        } else {
+            console.log('Incorrect password. Try again.');
+        }
+
+        counter++;
+    }
+}
+login(['Acer', 'login', 'go', 'let me in', 'recA']);
