a759a4814538df4b42cbccb46f271120f1a17293
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-18 12:59:05 +0200

    Update 01. Sort Numbers.js

--- 01. Basic-Syntax-Conditional-Statements-and-Loops/More-Exercises/01. Sort Numbers.js
+++ 01. Basic-Syntax-Conditional-Statements-and-Loops/More-Exercises/01. Sort Numbers.js
@@ -1,6 +1,44 @@
-function sortNumbers() {
+function sortNumbers(numbA, numbB, numbC) {
 
+    let max = numbA;
 
+    if (max < numbB) {
+        max = numbB;
+    }
 
+    if (max < numbC) {
+        max = numbC;
+    }
+    console.log(max);
+
+    if (max == numbA) {
+        if (numbB > numbC) {
+            console.log(numbB);
+            console.log(numbC);
+        } else {
+            console.log(numbC);
+            console.log(numbB);
+        }
+    }
+
+    if (max == numbB) {
+        if (numbA > numbC) {
+            console.log(numbA);
+            console.log(numbC);
+        } else {
+            console.log(numbC);
+            console.log(numbA);
+        }
+    }
+
+    if (max == numbC) {
+        if (numbA > numbB) {
+            console.log(numbA);
+            console.log(numbB);
+        } else {
+            console.log(numbB);
+            console.log(numbA);
+        }
+    }
 }
 sortNumbers(2, 1, 3);
