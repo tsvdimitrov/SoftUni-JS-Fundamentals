ff9bd6ec8b4848c77f994b95d5830a4d4dc96a47
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-18 22:02:15 +0200

    Create 03. Integer and Float.js

---
+++ 02. Data-Types-and-Variables/Lab/03. Integer and Float.js
@@ -0,0 +1,14 @@
+function integerOrFloat(a, b, c) {
+
+    let sum = a + b + c;
+    let rounded = Math.round(sum);
+
+    if (sum == round) {
+        console.log(`${sum} - Integer`);
+    } else {
+        console.log(`${sum} - Float`);
+    }
+
+
+}
+integerOrFloat(9, 100, 1.1);
