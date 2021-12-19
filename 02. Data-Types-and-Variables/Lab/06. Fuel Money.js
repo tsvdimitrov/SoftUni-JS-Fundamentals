5c70bbf0945c335daa5e80c90acf90dd27ffe73c
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-19 10:21:50 +0200

    Update 06. Fuel Money.js

--- 02. Data-Types-and-Variables/Lab/06. Fuel Money.js
+++ 02. Data-Types-and-Variables/Lab/06. Fuel Money.js
@@ -1,6 +1,10 @@
 function fuelMoney(distance, people, price) {
 
+    let consumption = 7 + (people * 0.1);
+    let fuel = distance / 100 * consumption;
+    let total = fuel * price;
 
+    console.log(`Needed money for that trip is ${total} lv`);
 
 }
 fuelMoney(260, 9, 2.49);
