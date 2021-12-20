9bbac98f358be88ebcf5ee11bcf30ff2bbc92edb
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-20 21:26:56 +0200

    Update 03. Reverse an Array of Numbers.js

--- 03. Arrays/Lab/03. Reverse an Array of Numbers.js
+++ 03. Arrays/Lab/03. Reverse an Array of Numbers.js
@@ -1,15 +1,9 @@
 function reverseAnArrayOfNumbers(n, array) {
 
-    let slicedArray = [];
-
-    for (let i = 0; i < n; i++) {
-        slicedArray.push(array[i]);
-    }
-
     let result = [];
 
-    for (let i = slicedArray.length - 1; i >= 0; i--) {
-        result.push(slicedArray[i]);
+    for (let i = n - 1; i >= 0; i--) {
+        result.push(array[i]);
     }
 
     console.log(result.join(' '));
