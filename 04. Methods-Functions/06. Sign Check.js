c145b0e5366e9a1cf02bcf69bb1c4d20ba7adec7
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-24 11:20:21 +0200

    Create 06. Sign Check.js

---
+++ 04. Methods-Functions/06. Sign Check.js
@@ -0,0 +1,27 @@
+function signCheck(numOne, numTwo, numThree) {
+
+    let result = '';
+
+    if (numOne >= 0 && numTwo >= 0 && numThree >= 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo < 0 && numThree >= 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo < 0 && numThree < 0) {
+        result = 'Negative';
+    } else if (numOne >= 0 && numTwo >= 0 && numThree < 0) {
+        result = 'Negative';
+    } else if (numOne < 0 && numTwo >= 0 && numThree >= 0) {
+        result = 'Negative';
+    } else if (numOne < 0 && numTwo >= 0 && numThree < 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo >= 0 && numThree >= 0) {
+        result = 'Negative';
+    } else if (numOne >= 0 && numTwo < 0 && numThree < 0) {
+        result = 'Positive';
+    } else if (numOne >= 0 && numTwo < 0 && numThree >= 0) {
+        result = 'Negative';
+    }
+
+    console.log(result);
+}
+signCheck(5, 12, -15);
