f8744d5362a30d0442bac31d08951e013cad1c63
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-25 14:33:16 +0200

    Create 04. Last K Numbers Sequence.js

---
+++ 05. Arrays-Advanced/Lab/04. Last K Numbers Sequence.js
@@ -0,0 +1,18 @@
+function lastKNumbersSequence(n, k) {
+
+    let result = [1];
+
+    for (let i = 1; i < n; i++) {
+
+        let current = 0;
+        let element = result.slice(-k);
+
+        for (let num of elements) {
+            current += num;
+        }
+        result.push(current);
+    }
+
+    console.log(result.join(' '));
+}
+lastKNumbersSequence(6, 3);
