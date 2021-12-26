866a894fe75167ca9f64c3bb263e5f8f507b69da
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-26 19:20:55 +0200

    Create 05. Cats.js

---
+++ 06. Objects-and-Classes/Lab/05. Cats.js
@@ -0,0 +1,28 @@
+function cats(catsAsStrings) {
+
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    let cats = [];
+
+    for (let catData of catsAsStrings) {
+        let [name, age] = catData.split(' ');
+
+        let cat = new Cat(name, age);
+        cats.push(cat);
+    }
+
+    for (let cat of cats) {
+        cat.meow();
+    }
+}
+cats(['Mellow 2', 'Tom 5']);
