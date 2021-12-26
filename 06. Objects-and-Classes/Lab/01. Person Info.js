d800e7fcb6c9537477d0c7c88c75f4c5f4c7d3c9
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-26 10:26:12 +0200

    Update 01. Person Info.js

--- 06. Objects-and-Classes/Lab/01. Person Info.js
+++ 06. Objects-and-Classes/Lab/01. Person Info.js
@@ -1,6 +1,11 @@
 function personInfo(firstName, lastName, age) {
 
-    let person
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
 
+    return person;
 }
-personInfo("Peter", "Pan", "20");
+console.log(personInfo("Peter", "Pan", "20"));
