c1353ae94b1b52f907979bca11106c0e37d66405
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-27 10:25:31 +0200

    Create 01. Phone Book.js

---
+++ 07. Associative-Arrays/01. Phone Book.js
@@ -0,0 +1,21 @@
+function phoneBook(input) {
+
+    let phonebook = {};
+
+    for (let line of input) {
+
+        let tokens = line.split(' ');
+        let name = tokens[0];
+        let phone = tokens[1];
+
+        phonebook[name] = phone;
+    }
+
+    for (let name in phonebook) {
+        console.log(`${name} -> ${phonebook[name]}`);
+    }
+}
+phoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']);
