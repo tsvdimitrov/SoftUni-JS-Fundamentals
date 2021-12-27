112e086505071e6c4b3e3e24c8d8128775159239
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-27 12:43:27 +0200

    Update 07. Neighborhoods.js

---
+++ 07. Associative-Arrays/07. Neighborhoods.js
@@ -0,0 +1,35 @@
+function neighborhoods(input) {
+
+    let map = new Map();
+    let neighborhoods = input.shift().split(', ');
+
+    for (let neighborhood of neighborhoods) {
+        map.set(neighborhood, []);
+    }
+
+    while (input.length > 0) {
+        let current = input.shift().split(' - ');
+        let neighborhood = current[0];
+        let person = current[1];
+        if (neighborhoods.includes(neighborhood)) {
+            map.get(neighborhood).push(person);
+        }
+    }
+
+    let sorted = Array.from(map)
+        .sort((a, b) => { return b[1].length - a[1].length; });
+
+    for (let neighborhood of sorted) {
+        let neighborhoodName = neighborhood[0];
+        let persons = neighborhood[1];
+        console.log(`${neighborhoodName}: ${persons.length}`);
+        for (let j = 0; j < persons.length; j++) {
+            console.log(`--${persons[j]}`);
+        }
+    }
+}
+neighborhoods(['Abbey Street, Herald Street, Bright Mews',
+    'Bright Mews - Garry',
+    'Bright Mews - Andrea',
+    'Invalid Street - Tommy',
+    'Abbey Street - Billy']);
