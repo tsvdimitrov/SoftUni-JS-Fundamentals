d312e30fb3c8b0808f4ee6debf4605fe40445317
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-27 11:30:26 +0200

    Create 03. Address Book.js

---
+++ 07. Associative-Arrays/03. Address Book.js
@@ -0,0 +1,18 @@
+function addressBook(input) {
+
+    let catalog = {};
+
+    for (let line of input) {
+        let [name, address] = line.split(':');
+
+        catalog[name] = address;
+    }
+
+
+
+
+}
+addressBook(['Tim:Doe Crossing',
+    'Bill:Nelson Place',
+    'Peter:Carlyle Ave',
+    'Bill:Ornery Rd']);
