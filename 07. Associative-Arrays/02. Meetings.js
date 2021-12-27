2a4de8fdc73de8141b414a5ce5cc364548f3676d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-27 11:20:31 +0200

    Create 02. Meetings.js

---
+++ 07. Associative-Arrays/02. Meetings.js
@@ -0,0 +1,23 @@
+function meetings(input) {
+
+    let calendar = {};
+
+    for (let line of input) {
+        let [day, name] = line.split(' ');
+
+        if (calendar.hasOwnProperty(day)) {
+            console.log(`Conflict on ${day}!`);
+        } else {
+            console.log(`Scheduled for ${day}`);
+            calendar[day] = name;
+        }
+    }
+
+    for (let day in calendar) {
+        console.log(`${day} -> ${calendar[day]}`);
+    }
+}
+meetings(['Monday Peter',
+    'Wednesday Bill',
+    'Monday Tim',
+    'Friday Tim']);
