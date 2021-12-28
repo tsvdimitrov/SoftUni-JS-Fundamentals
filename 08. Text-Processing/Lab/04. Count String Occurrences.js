b83506f1bb9ebf1e823a8736bf205d37ce68aabf
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-28 12:29:04 +0200

    Update 04. Count String Occurrences.js

--- 08. Text-Processing/Lab/04. Count String Occurrences.js
+++ 08. Text-Processing/Lab/04. Count String Occurrences.js
@@ -1,6 +1,14 @@
-function countStringOccurences(text, word) {
+function countStringOccurences(text, string) {
+    
+    let splitted = text.split(" ");
+    let counter = 0;
 
+    for (let word of splitted) {
+        if (word === string) {
+            counter++;
+        }
+    }
 
-
+    console.log(counter);
 }
 countStringOccurences("This is a word and it also is a sentence", "is");
