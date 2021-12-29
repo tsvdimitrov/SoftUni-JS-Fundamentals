6c5a52162127b202ce32ea626b0c4314c541202d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-29 19:11:01 +0200

    Create 04. Reverse String.js

---
+++ 01. Basic-Syntax-Conditional-Statements-and-Loops/More-Exercises/04. Reverse String.js
@@ -0,0 +1,11 @@
+function reverseStrng(input) {
+
+    let output = "";
+
+    for (let i = input.length - 1; i >= 0; i--) {
+        output += input[i];
+    }
+
+    console.log(output);
+}
+reverseStrng('Hello');
