70e64d9eaf570f97587f75d78899e9c30b3b672d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-29 11:21:17 +0200

    Update 03. Match Dates.js

--- 09. Regular-Expressions-Lab/Lab/03. Match Dates.js
+++ 09. Regular-Expressions-Lab/Lab/03. Match Dates.js
@@ -1,6 +1,14 @@
-function matchDates() {
+function matchDates(input) {
 
+    let pattern = /\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})\b/g;
 
+    while ((validDate = pattern.exec(input)) !== null) {
 
+        let day = validDate.groups['day'];
+        let month = validDate.groups['month'];
+        let year = validDate.groups['year'];
+
+        console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+    }
 }
 matchDates();
