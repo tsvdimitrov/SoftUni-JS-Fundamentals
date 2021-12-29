c536a0bc4542ef6a570cd0717b9fda28490307f1
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-29 11:21:45 +0200

    Update 03. Match Dates.js

--- 09. Regular-Expressions-Lab/Lab/03. Match Dates.js
+++ 09. Regular-Expressions-Lab/Lab/03. Match Dates.js
@@ -11,4 +11,4 @@ function matchDates(input) {
         console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
     }
 }
-matchDates();
+matchDates("13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016");
