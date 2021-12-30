0d116446eddfa112570ef85559941366646112f1
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2021-12-30 09:16:36 +0200

    Update 03. Town Info.js

--- 02. Data-Types-and-Variables/Exercise/03. Town Info.js
+++ 02. Data-Types-and-Variables/Exercise/03. Town Info.js
@@ -2,4 +2,4 @@ function townInfo(town, population, area) {
 
     console.log(`Town ${town} has population of ${population} and area ${area} square km.`)
 }
-townInfo();
+townInfo('Sofia', 1286383, 492);
