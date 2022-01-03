e817b286028f035f6380945624572362d3173147
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-03 20:48:07 +0200

    Create 10. Ladybugs.js

---
+++ 03. Arrays/Exercise/10. Ladybugs.js
@@ -0,0 +1,45 @@
+function ladybugs(arr) {
+
+    let field = new Array(arr.shift()).fill(0);
+    const indexes = arr
+        .shift()
+        .split(" ")
+        .map(x => Number(x))
+    indexes.forEach(x => {
+        if (field[x] !== undefined) field[x] = 1
+    })
+
+    function moveLeft(arr, index) {
+        for (let i = index; i >= 0; i -= index) {
+            if (arr[i] === 0) {
+                arr[i] = 1;
+                break;
+            }
+        }
+        return arr;
+    }
+
+    function moveRight(arr, index) {
+        for (let i = index; i < arr.length; i += index) {
+            if (arr[i] === 0) {
+                arr[i] = 1
+                break
+            }
+        }
+        return arr;
+    }
+    arr.forEach(x => {
+        const [startIndex, direction, endIndex] = x.split(" ")
+        if (field[Number(startIndex)] !== 0 && field[Number(startIndex)] !== undefined) {
+            field[Number(startIndex)] = 0
+            direction === "left"
+                ? (field = moveLeft(field, Number(startIndex) - Number(endIndex)))
+                : (field = moveRight(field, Number(startIndex) + Number(endIndex)))
+        }
+    })
+
+    console.log(field.join(" "));
+}
+ladybugs([3, '0 1',
+    '0 right 1',
+    '2 right 1']);
