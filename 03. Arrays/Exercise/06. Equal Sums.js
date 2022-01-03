ef5d4431419f433668a99388ea3281bb9b169a2e
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-03 20:40:17 +0200

    Create 06. Equal Sums.js

---
+++ 03. Arrays/Exercise/06. Equal Sums.js
@@ -0,0 +1,30 @@
+function equalSums(list) {
+
+    let hasFoundResult = false;
+
+    for (let i = 0; i < list.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+
+        for (let leftIdx = 0; leftIdx < i; leftIdx++) {
+            let currentNumber = list[leftIdx];
+            leftSum += currentNumber;
+        }
+
+        for (let rightIdx = i + 1; rightIdx < list.length; rightIdx++) {
+            let currentNumber = list[rightIdx];
+            rightSum += currentNumber;
+        }
+
+        if (leftSum === rightSum) {
+            console.log(i);
+            hasFoundResult = true;
+            break;
+        }
+    }
+
+    if (!hasFoundResult) {
+        console.log('no');
+    }
+}
+equalSums([1, 2, 3, 3]);
