7a6ae72f3528345b1def74b2669444217a3085cc
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-05 21:07:35 +0200

    Update 10. Factorial Division.js

--- 04. Methods-Functions/Exercise/10. Factorial Division.js
+++ 04. Methods-Functions/Exercise/10. Factorial Division.js
@@ -1,10 +1,11 @@
 function foo(...args) {
-    let result = []
+
+    let result = [];
     args.forEach((x, i) => {
         for (let j = x; j > 0; j--) {
-            result[i] = j * (result[i] || 1)
+            result[i] = j * (result[i] || 1);
         }
     })
 
-    console.log((result[0] / result[1]).toFixed(2))
+    console.log((result[0] / result[1]).toFixed(2));
 }
