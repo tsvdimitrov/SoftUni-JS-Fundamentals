faab04e0b599eb139209fd7cbd9c621de22f27cd
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-05 21:01:13 +0200

    Create 7. NxN Matrix.js

---
+++ 04. Methods-Functions/Exercise/7. NxN Matrix.js
@@ -0,0 +1,14 @@
+function NxNMatrix(n) {
+
+    let row = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            row.push(n);
+        }
+
+        console.log(row.join(" "));
+        row = [];
+    }
+}
+NxNMatrix(7);
