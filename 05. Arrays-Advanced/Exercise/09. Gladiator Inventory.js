28fd5991af3cedc8da9c1f69c494e02df4575f2d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-06 21:01:11 +0200

    Update 09. Gladiator Inventory.js

--- 05. Arrays-Advanced/Exercise/09. Gladiator Inventory.js
+++ 05. Arrays-Advanced/Exercise/09. Gladiator Inventory.js
@@ -1,42 +1,49 @@
-function foo(arr) {
-    let items = arr.shift().split(" ")
+function gladiatorInventory(arr) {
+
+    let items = arr.shift().split(" ");
+
     class Inventory {
         constructor(items) {
-            this.items = items
+            this.items = items;
         }
 
-        getIndex = e => this.items.findIndex(x => x === e)
+        getIndex = e => this.items.findIndex(x => x === e);
 
         Buy = i => {
-            if (!this.items.includes(i)) this.items.push(i)
+            if (!this.items.includes(i)) this.items.push(i);
         }
 
         Trash = e => {
-            if (this.items.includes(e)) this.items = this.items.filter(x => x !== e)
+            if (this.items.includes(e)) this.items = this.items.filter(x => x !== e);
         }
 
         Repair = e => {
             if (this.items.includes(e)) {
-                const index = this.getIndex(e)
-                const result = this.items.splice(index, 1)
-                this.items.push(result[0])
+                const index = this.getIndex(e);
+                const result = this.items.splice(index, 1);
+                this.items.push(result[0]);
             }
         }
 
         Upgrade = x => {
-            const [item, upgrade] = x.split("-")
+            const [item, upgrade] = x.split("-");
             if (this.items.includes(item)) {
-                const index = this.getIndex(item)
-                this.items.splice(index + 1, 0, `${item}:${upgrade}`)
+                const index = this.getIndex(item);
+                this.items.splice(index + 1, 0, `${item}:${upgrade}`);
             }
         }
     }
 
-    const currentInventory = new Inventory(items)
+    const currentInventory = new Inventory(items);
     arr.forEach(x => {
-        const [action, item] = x.split(" ")
-        currentInventory[action](item)
+        const [action, item] = x.split(" ");
+        currentInventory[action](item);
     })
 
-    console.log(currentInventory.items.join(' '))
+    console.log(currentInventory.items.join(' '));
 }
+gladiatorInventory(['SWORD Shield Spear',
+    'Buy Bag',
+    'Trash Shield',
+    'Repair Spear',
+    'Upgrade SWORD-Steel']);
