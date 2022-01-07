8bcf6659000c7a964638b5894836307e2f04bb18
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-07 21:14:25 +0200

    Update 07. Class Vehicle.js

--- 06. Objects-and-Classes/Exercise/07. Class Vehicle.js
+++ 06. Objects-and-Classes/Exercise/07. Class Vehicle.js
@@ -17,3 +17,9 @@ class Vehicle {
 
     drive = x => (this.fuel -= x)
 }
+
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
