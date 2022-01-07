8e1f476d481ec7d2eb287798c5612d2a1c320623
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-07 21:05:41 +0200

    Update 04. Movies.js

--- 06. Objects-and-Classes/Exercise/04. Movies.js
+++ 06. Objects-and-Classes/Exercise/04. Movies.js
@@ -1,7 +1,8 @@
 function foo(arr) {
-    let movies = []
 
-    getMovie = value => movies.find(x => x.name === value)
+    let movies = [];
+
+    getMovie = value => movies.find(x => x.name === value);
 
     addProp = (prop, name, value) => {
         if (getMovie(name)) getMovie(name)[prop] = value
@@ -15,18 +16,18 @@ function foo(arr) {
 
     arr.forEach(x => {
         if (x.split(" ")[0] === "addMovie") {
-            let arr = x.split(" ")
-            arr.shift()
-            actions.addMovie(arr.join(" "))
+            let arr = x.split(" ");
+            arr.shift();
+            actions.addMovie(arr.join(" "));
         } else if (x.split("directedBy").length > 1) {
-            let array = x.split("directedBy").map(x => x.trim())
-            actions.directedBy(array[0], array[1])
+            let array = x.split("directedBy").map(x => x.trim());
+            actions.directedBy(array[0], array[1]);
         } else if (x.split("onDate").length > 1) {
-            let array = x.split("onDate").map(x => x.trim())
-            actions.onDate(array[0], array[1])
+            let array = x.split("onDate").map(x => x.trim());
+            actions.onDate(array[0], array[1]);
         }
     })
 
-    movies = movies.filter(x => Object.keys(x).length === 3)
-    movies.forEach(x => console.log(JSON.stringify(x)))
+    movies = movies.filter(x => Object.keys(x).length === 3);
+    movies.forEach(x => console.log(JSON.stringify(x)));
 }
