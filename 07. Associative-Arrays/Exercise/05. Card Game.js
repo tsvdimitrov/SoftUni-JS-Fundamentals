c826eb35ae6afd8b34124b28e6535894efcb83fa
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-08 17:43:40 +0200

    Create 05. Card Game.js

---
+++ 07. Associative-Arrays/Exercise/05. Card Game.js
@@ -0,0 +1,50 @@
+function cardGame(input) {
+
+    let type = {
+        S: 4,
+        H: 3,
+        D: 2,
+        C: 1
+    };
+
+    let power = {
+        J: 11,
+        Q: 12,
+        K: 13,
+        A: 14
+    };
+
+    let players = {};
+
+    for (const line of input) {
+        let [name, cards] = line.split(": ");
+        if (players.hasOwnProperty(name)) {
+            players[name].push(...cards.split(", "));
+        } else {
+            players[name] = cards.split(", ");
+        }
+    }
+    Object.keys(players).forEach(name => {
+
+        let cardSet = new Set(players[name]);
+
+        let total = 0;
+        for (let card of cardSet) {
+            card = card.split("");
+            let cardType = card.pop();
+            let cardPower = card.join("");
+            let points = 0;
+
+            if (isNaN(cardPower)) {
+                points = type[cardType] * power[cardPower];
+            } else {
+                points = type[cardType] * Number(cardPower);
+            }
+            total += points;
+        }
+        players[name] = total;
+    });
+
+    return Object.keys(players).map(name => `${name}: ${players[name]}`).join("\n");
+}
+cardGame();
