cf72bc29ca609b794b72f90e4aa5d0617e667cf9
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-08 17:44:23 +0200

    Update 05. Card Game.js

--- 07. Associative-Arrays/Exercise/05. Card Game.js
+++ 07. Associative-Arrays/Exercise/05. Card Game.js
@@ -47,4 +47,10 @@ function cardGame(input) {
 
     return Object.keys(players).map(name => `${name}: ${players[name]}`).join("\n");
 }
-cardGame();
+console.log(cardGame([
+    'Peter: 2C, 4H, 9H, AS, QS',
+    'Tomas: 3H, 10S, JC, KD, 5S, 10S',
+    'Andrea: QH, QC, QS, QD',
+    'Tomas: 6H, 7S, KC, KD, 5S, 10C',
+    'Andrea: QH, QC, JS, JD, JC',
+    'Peter: JD, JD, JD, JD, JD, JD']));
