19d1b3a695e64d1aace2bb22c9bf35e0512d0134
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-08 17:50:47 +0200

    Update 07. Company Users.js

--- 07. Associative-Arrays/Exercise/07. Company Users.js
+++ 07. Associative-Arrays/Exercise/07. Company Users.js
@@ -23,4 +23,8 @@ function companyUsers(input) {
         }
     }
 }
-companyUsers();
+companyUsers([
+    'SoftUni -> AA12345',
+    'SoftUni -> BB12345',
+    'Microsoft -> CC12345',
+    'HP -> BB12345']);
