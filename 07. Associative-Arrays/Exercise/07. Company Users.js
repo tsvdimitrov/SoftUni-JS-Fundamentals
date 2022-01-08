c28005bba73d37e72e265e2cc0b60be30d93f4a8
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-08 17:49:56 +0200

    Create 07. Company Users.js

---
+++ 07. Associative-Arrays/Exercise/07. Company Users.js
@@ -0,0 +1,26 @@
+function companyUsers(input) {
+    let companies = {}
+
+
+    for (let i = 0; i < input.length; i++) {
+        let [company, id] = input[i].split(" -> ");
+
+        if (!companies.hasOwnProperty(company)) {
+            companies[company] = [];
+        }
+        companies[company].push(id);
+    }
+
+    let sorted = Object.entries(companies);
+    sorted.sort((a, b) => a[0].localeCompare(b[0]));
+
+
+    for (const elem of sorted) {
+        console.log(elem[0]);
+        let set = new Set(elem[1]);
+
+        for (let number of set) {
+            console.log(`-- ${number}`);
+        }
+    }
+}
