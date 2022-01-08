40df49097048992be611b2126192b30e99289f10
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-08 17:42:13 +0200

    Update 04. Party Time.js

--- 07. Associative-Arrays/Exercise/04. Party Time.js
+++ 07. Associative-Arrays/Exercise/04. Party Time.js
@@ -46,4 +46,12 @@ function partyTime(input) {
         return !isNaN(Number(guest[0]));
     }
 }
-partyTime();
+partyTime(['7IK9Yo0h',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc',
+    'tSzE5t0p',
+    'PARTY',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc']);
