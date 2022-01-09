a271b35900975fa6182f84555e312f64d41dcacf
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-09 15:54:41 +0200

    Update 02. Modern Times of #(HashTag).js

---
+++ 08. Text-Processing/Exercise/02. Modern Times of #(HashTag).js
@@ -0,0 +1,23 @@
+function modernTimesOfHashTag(string) {
+
+    let words = string.split(" ");
+
+    for (const word of words) {
+        if (word.startsWith(`#`) && isOnlyLetters(word.substring(1)) && word.substring(1) !== "") {
+            console.log(word.substring(1));
+        }
+    }
+
+    function isOnlyLetters(word) {
+        let isLetter = true;
+
+
+        for (const char of word) {
+            if (char.charCodeAt() < 65 || char.charCodeAt() > 90 && char.charCodeAt() < 97 || char.charCodeAt() > 122) {
+                isLetter = false;
+                break;
+            }
+        }
+        return isLetter;
+    }
+}
