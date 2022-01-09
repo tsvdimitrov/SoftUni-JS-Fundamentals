82715b07d913cf1e8211e596618e430086a18747
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-09 15:53:01 +0200

    Update 01. Reveal Words.js

---
+++ 08. Text-Processing/Exercise/01. Reveal Words.js
@@ -0,0 +1,14 @@
+function revealWords(words, text) {
+
+    text = ` ${text} `;
+    words = words.split(', ');
+
+    for (let word of words) {
+
+        let match = ` ${'*'.repeat(word.length)} `;
+        word = ` ${word} `;
+        text = text.replace(match, word);
+    }
+
+    console.log(text.trim());
+}
