a04741e873661dcd4c0fd80adb6706222fd08f43
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-10 19:27:04 +0200

    Update 03. SoftUni Bar Income.js

---
+++ 09. Regular-Expressions/Exercise/03. SoftUni Bar Income.js
@@ -0,0 +1,17 @@
+function softUniBarIncome(input) {
+
+    let income = 0;
+
+    for (const line of input) {
+        const regex = /%(?<name>[A-Z][a-z]+)%([^\|\$\%\.]+)?<(?<product>\w+)>([^\|\$\%\.]+)?\|(?<count>\d+)\|([^\|\$\%\.\d]+)?(?<price>\d+(\.\d+)?)\$/g
+        let match = regex.exec(line);
+
+        if (match) {
+            let totalPrice = Number(match.groups.count) * Number(match.groups.price);
+            income += totalPrice;
+            console.log(`${match.groups.name}: ${match.groups.product} - ${totalPrice.toFixed(2)}`)
+        }
+    }
+
+    console.log(`Total income: ${income.toFixed(2)}`);
+}
