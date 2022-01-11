af183cd656bbbf8d0dacc22aaa8436a699bfb4be
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-11 19:48:24 +0200

    Update 03. Cone.js

---
+++ 02. Data-Types-and-Variables/More-Exercises/03. Cone.js
@@ -0,0 +1,8 @@
+function cone(radius, height) {
+
+    let volume = (Math.PI * radius ** 2 * height) / 3;
+    let area = Math.PI * radius ** 2 + Math.PI * radius * Math.sqrt(radius ** 2 + height ** 2);
+
+    console.log(`volume = ${volume.toFixed(4)}`);
+    console.log(`area = ${area.toFixed(4)}`);
+}
