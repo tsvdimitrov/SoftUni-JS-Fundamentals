b03144896536c91dafd4823e8dedc101cc43cea5
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-11 19:51:08 +0200

    Update 05. Binary to Decimal.js

---
+++ 02. Data-Types-and-Variables/More-Exercises/05. Binary to Decimal.js
@@ -0,0 +1,14 @@
+function binaryToDecimal(binary) {
+
+    let decimal = 0;
+    let degreeOfTwo = 0;
+    let currentDigit = 0;
+
+    for (let i = binary.length - 1; i >= 0; i--) {
+        currentDigit = Number(binary[i]);
+        decimal += currentDigit * 2 ** degreeOfTwo;
+        degreeOfTwo++;
+    }
+    
+    console.log(decimal);
+}
