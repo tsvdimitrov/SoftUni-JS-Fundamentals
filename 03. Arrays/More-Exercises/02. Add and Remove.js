e2eb63e6de2c44741a5dabcaa73b0c143f8e48fa
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-12 19:57:10 +0200

    Update 02. Add and Remove.js

---
+++ 03. Arrays/More-Exercises/02. Add and Remove.js
@@ -0,0 +1,18 @@
+function solve(input) {
+
+    let result = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == "add") {
+            result.push(i + 1);
+        } else {
+            result.pop();
+        }
+    }
+
+    if (result != 0) {
+        console.log(result.join(' '));
+    } else {
+        console.log("Empty");
+    }
+}
