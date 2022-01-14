bfc0a82cad67763cfbd881bedd07660678c6d0e0
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-14 21:54:59 +0200

    Update 01. Equal Neighbors.js

--- 05. Arrays-Advanced/More-Exercises/01. Equal Neighbors.js
+++ 05. Arrays-Advanced/More-Exercises/01. Equal Neighbors.js
@@ -42,4 +42,7 @@ function neighbourFinder(arrInput) {
 
     return counter;
 }
-console.log(neighbourFinder());
+console.log(neighbourFinder([['2', '3', '4', '7', '0'],
+['4', '0', '5', '3', '4'],
+['2', '3', '5', '4', '2'],
+['9', '8', '7', '5', '4']]));
