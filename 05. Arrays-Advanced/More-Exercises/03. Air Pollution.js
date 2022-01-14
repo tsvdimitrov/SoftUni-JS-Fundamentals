5d9f5cb7ef6e76681650128b843ea6927a24b08b
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-14 21:58:13 +0200

    Update 03. Air Pollution.js

--- 05. Arrays-Advanced/More-Exercises/03. Air Pollution.js
+++ 05. Arrays-Advanced/More-Exercises/03. Air Pollution.js
@@ -45,4 +45,9 @@ function airPollution(matrixArrInput, forcesArrInput) {
         console.log('No polluted areas');
     }
 }
-airPollution();
+airPollution(["5 7 72 14 4",
+    "41 35 37 27 33",
+    "23 16 27 42 12",
+    "2 20 28 39 14",
+    "16 34 31 10 24"],
+    ["breeze 1", "gale 2", "smog 25"]);
