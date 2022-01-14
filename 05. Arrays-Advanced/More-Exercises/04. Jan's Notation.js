c19d79388b113807874518167a823b757a89f680
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-14 21:59:20 +0200

    Update 04. Jan's Notation.js

---
+++ 05. Arrays-Advanced/More-Exercises/04. Jan's Notation.js
@@ -0,0 +1,65 @@
+function solve(arrInput) {
+
+    let operandsArr = [];
+
+    let firstOperand = 0;
+    let secondOperand = 0;
+    let currentResult = 0;
+
+    let flag = true;
+
+    for (const element of arrInput) {
+
+        if (typeof element === 'number') {
+            operandsArr.push(element);
+        } else {
+            operator = element;
+
+            if (operandsArr.length <= 1) {
+                console.log('Error: not enough operands!');
+                flag = false;
+                break;
+            }
+
+            if ((operator === '+')) {
+                firstOperand = operandsArr[operandsArr.length - 2];
+                secondOperand = operandsArr[operandsArr.length - 1];
+
+                operandsArr.length -= 2;
+
+                currentResult = firstOperand + secondOperand;
+                operandsArr.push(currentResult);
+            } else if ((operator === '-')) {
+                firstOperand = operandsArr[operandsArr.length - 2];
+                secondOperand = operandsArr[operandsArr.length - 1];
+
+                operandsArr.length -= 2;
+
+                currentResult = firstOperand - secondOperand;
+                operandsArr.push(currentResult);
+            } else if ((operator === '*')) {
+                firstOperand = operandsArr[operandsArr.length - 2];
+                secondOperand = operandsArr[operandsArr.length - 1];
+
+                operandsArr.length -= 2;
+
+                currentResult = firstOperand * secondOperand;
+                operandsArr.push(currentResult);
+            } else if ((operator === '/')) {
+                firstOperand = operandsArr[operandsArr.length - 2];
+                secondOperand = operandsArr[operandsArr.length - 1];
+
+                operandsArr.length -= 2;
+
+                currentResult = firstOperand / secondOperand;
+                operandsArr.push(currentResult);
+            }
+        }
+    }
+
+    if (operandsArr.length > 1 && flag) {
+        console.log('Error: too many operands!');
+    } else if (flag) {
+        console.log(operandsArr[0]);
+    }
+}
