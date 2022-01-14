04fdb47197ea048e35c2bf0914115e99c4466f14
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-14 22:02:38 +0200

    Update 05. Rosetta Stone.js

--- 05. Arrays-Advanced/More-Exercises/05. Rosetta Stone.js
+++ 05. Arrays-Advanced/More-Exercises/05. Rosetta Stone.js
@@ -36,4 +36,11 @@ function rosettaStone(input) {
 
     console.log(encodedMessage.trim());
 }
-rosettaStone();
+rosettaStone(['2',
+    '59 36',
+    '82 52',
+    '4 18 25 19 8',
+    '4 2 8 2 18',
+    '23 14 22 0 22',
+    '2 17 13 19 20',
+    '0 9 0 22 22']);
