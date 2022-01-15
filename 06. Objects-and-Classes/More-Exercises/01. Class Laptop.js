b34a6f47b202a204fd7e576085efcf03be18b8f9
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-15 18:25:17 +0200

    Update 01. Class Laptop.js

--- 06. Objects-and-Classes/More-Exercises/01. Class Laptop.js
+++ 06. Objects-and-Classes/More-Exercises/01. Class Laptop.js
@@ -13,3 +13,13 @@ class Laptop {
         return (800 - this.info.producer.age * 2 + (this.quality * 0.5))
     }
 }
+
+let info = { producer: "Dell", age: 2, brand: "XPS" };
+let laptop = new Laptop(info, 10);
+laptop.turnOn();
+console.log(laptop.showInfo());
+laptop.turnOff();
+console.log(laptop.quality);
+laptop.turnOn();
+console.log(laptop.isOn);
+console.log(laptop.price);
