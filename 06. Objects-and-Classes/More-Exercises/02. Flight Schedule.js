220fa3e7a953d922295401659bbba55b835f85dc
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-15 18:27:05 +0200

    Update 02. Flight Schedule.js

--- 06. Objects-and-Classes/More-Exercises/02. Flight Schedule.js
+++ 06. Objects-and-Classes/More-Exercises/02. Flight Schedule.js
@@ -1,5 +1,5 @@
 function flightSchedule(input) {
-    
+
     let flights = {};
 
     input[0].forEach(line => {
@@ -17,10 +17,24 @@ function flightSchedule(input) {
         }
     });
 
-
     for (let fly in flights) {
         if (flights[fly].Status === String(input[2])) {
             console.log(flights[fly]);
         }
     }
 }
+flightSchedule([['WN269 Delaware',
+    'FL2269 Oregon',
+    'WN498 Las Vegas',
+    'WN3145 Ohio',
+    'WN612 Alabama',
+    'WN4010 New York',
+    'WN1173 California',
+    'DL2120 Texas',
+    'KL5744 Illinois',
+    'WN678 Pennsylvania'],
+['DL2120 Cancelled',
+    'WN612 Cancelled',
+    'WN1173 Cancelled',
+    'SK430 Cancelled'],
+['Cancelled']]);
