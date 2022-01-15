206dd0dfc53698f0265605b929306443356e25fa
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-15 18:30:15 +0200

    Update 04. Browser History.js

---
+++ 06. Objects-and-Classes/More-Exercises/04. Browser History.js
@@ -0,0 +1,38 @@
+function browserHistory() {
+
+    let index = 0;
+    let input = arguments;
+    let currentBrowserState = input[index++];
+    let procedures = input[index++];
+
+    procedures.forEach(procedure => {
+        procedure = procedure.split(" ");
+        let keyWord = procedure.shift();
+        let remaining = procedure.join(" ");
+
+        procedure.unshift(keyWord);
+        if (keyWord === "Open") {
+            currentBrowserState["Open Tabs"].push(remaining);
+            currentBrowserState["Browser Logs"].push(procedure.join(" "));
+        } else if (keyWord === "Close") {
+            currentBrowserState["Open Tabs"].forEach((tab) => {
+                if (tab === remaining) {
+                    currentBrowserState["Open Tabs"].splice(currentBrowserState["Open Tabs"].indexOf(remaining), 1);
+
+                    currentBrowserState["Recently Closed"].push(remaining);
+                    currentBrowserState["Browser Logs"].push(procedure.join(" "));
+
+                }
+            })
+        } else if (keyWord === "Clear") {
+            currentBrowserState["Open Tabs"] = [];
+            currentBrowserState["Recently Closed"] = [];
+            currentBrowserState["Browser Logs"] = [];
+        }
+    });
+
+    console.log(currentBrowserState["Browser Name"]);
+    console.log(`Open Tabs: ${currentBrowserState["Open Tabs"].join(", ")}`);
+    console.log(`Recently Closed: ${currentBrowserState["Recently Closed"].join(", ")}`);
+    console.log(`Browser Logs: ${currentBrowserState["Browser Logs"].join(", ")}`);
+}
