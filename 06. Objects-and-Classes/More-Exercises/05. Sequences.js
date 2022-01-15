581495bf019679ea442e5fca543a6f7f883101de
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-15 18:34:32 +0200

    Update 05. Sequences.js

--- 06. Objects-and-Classes/More-Exercises/05. Sequences.js
+++ 06. Objects-and-Classes/More-Exercises/05. Sequences.js
@@ -20,9 +20,11 @@ function sequences(input) {
             outputArr.push(currentArray);
         }
     }
+
     outputArr.sort((a, b) => a.length - b.length);
     outputArr.forEach(el => console.log(`[${el.join(", ")}]`));
 }
+
 sequences(["[-3, -2, -1, 0, 1, 2, 3, 4]",
     "[10, 1, -17, 0, 2, 13]",
     "[4, -3, 3, -2, 2, -1, 1, 0]"]);
