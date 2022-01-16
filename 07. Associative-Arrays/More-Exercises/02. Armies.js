956ea6c3da3a3673a0e630c40be9777a2d89d97a
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-16 18:03:34 +0200

    Update 02. Armies.js

--- 07. Associative-Arrays/More-Exercises/02. Armies.js
+++ 07. Associative-Arrays/More-Exercises/02. Armies.js
@@ -55,3 +55,4 @@ function armies(input) {
                 });
         });
 }
+armies(['Rick Burr arrives', 'Fergus: Wexamp, 30245', 'Rick Burr: Juard, 50000', 'Findlay arrives', 'Findlay: Britox, 34540', 'Wexamp + 6000', 'Juard + 1350', 'Britox + 4500', 'Porter arrives', 'Porter: Legion, 55000', 'Legion + 302', 'Rick Burr defeated', 'Porter: Retix, 3205']);
