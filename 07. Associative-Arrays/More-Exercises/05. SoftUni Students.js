2e64611132ab61fecaca579f43509dade7146f86
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-16 18:07:39 +0200

    Update 05. SoftUni Students.js

--- 07. Associative-Arrays/More-Exercises/05. SoftUni Students.js
+++ 07. Associative-Arrays/More-Exercises/05. SoftUni Students.js
@@ -24,6 +24,7 @@ function softUniStudents(input) {
             }
         }
     }
+    
     Object.keys(courses).sort((a, b) => courses[b].users.length - courses[a].users.length).map(k => {
         courses[k].users = courses[k].users.sort((a, b) => b.credits - a.credits);
         return k;
@@ -33,6 +34,8 @@ function softUniStudents(input) {
         if (users.length > 0) {
             console.log(`${users.map(s => `--- ${s.credits}: ${s.userName}, ${s.email}`).join('\n')}`);
         }
+
     });
 }
+
 softUniStudents(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
