a24590f30c5c93eb8960698a5a7be9fde795759d
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-16 18:04:48 +0200

    Update 03. Comments.js

--- 07. Associative-Arrays/More-Exercises/03. Comments.js
+++ 07. Associative-Arrays/More-Exercises/03. Comments.js
@@ -1,5 +1,5 @@
 function comments(input) {
-    
+
     let users = [];
     let articles = [];
     for (let i = 0; i < input.length; i++) {
@@ -44,3 +44,4 @@ function comments(input) {
         }
     }
 }
+comments(['user aUser123', 'someUser posts on someArticle: NoTitle, stupidComment', 'article Books', 'article Movies', 'article Shopping', 'user someUser', 'user uSeR4', 'user lastUser', 'uSeR4 posts on Books: I like books, I do really like them', 'uSeR4 posts on Movies: I also like movies, I really do', 'someUser posts on Shopping: title, I go shopping every day', 'someUser posts on Movies: Like, I also like movies very much']);
