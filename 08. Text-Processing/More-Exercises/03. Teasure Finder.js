5dc88bbfc8308e78fb21847ae2fee73501c87bca
Tsvetomir Dimitrov <82708653+tsvdimitrov@users.noreply.github.com>
2022-01-17 20:18:19 +0200

    Update 03. Teasure Finder.js

---
+++ 08. Text-Processing/More-Exercises/03. Teasure Finder.js
@@ -0,0 +1,31 @@
+function treasureFinder(input) {
+
+    let key = input.shift().split(" ").map(Number);
+    let curPosition = 0;
+    let line;
+    let regex = /&(?<type>.+)&[^<]*<(?<coord>.+)>/g;
+    
+    while ((line = input.shift()) !== "find") {
+        let nums = line.split("");
+        curPosition = 0;
+        for (let i = 0; i < line.length; i++) {
+            if (curPosition >= key.length) {
+                curPosition = 0;
+            }
+            nums[i] = line.charCodeAt(i) - key[curPosition];
+            curPosition++;
+        }
+        let result = "";
+        for (let i = 0; i < nums.length; i++) {
+            result += String.fromCharCode(nums[i]);
+        }
+        let test = result.match(regex);
+        if (test) {
+            let match = regex.exec(result);
+            let type = match[1];
+            let coord = match[2];
+
+            console.log(`Found ${type} at ${coord}`);
+        }
+    }
+}
